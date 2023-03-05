6a7295d440c5536d7fb7f5c1a1883d8e7f56bab9
venubalan <venubalan55@gmail.com>
2023-03-05 20:58:50 +0530

    title added

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Chat App",
       theme: ThemeData(
         primaryColor: Constants().primaryColor
       ),
